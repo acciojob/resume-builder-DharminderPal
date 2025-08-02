86b85cc55152d05e57e68f95b719e84c769af228
Dharminder Pal (CBT) <cbt.sd4@deepakfasteners.in>
2025-08-02 14:54:35 +0530

    changes

--- src/store.js
+++ src/store.js
@@ -1,17 +1,20 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { legacy_createStore as createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from "./Reducer/index";
+import rootReducer from "./Reducer";
 
 const initialState = {};
 const middleware = [thunk];
 
+// Safe DevTools integration for both browser and test environments
+const composeEnhancers =
+  typeof window !== "undefined" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
+
 const store = createStore(
   rootReducer,
   initialState,
-  applyMiddleware(...middleware),
-  // compose(
-  //   applyMiddleware(...middleware),
-  //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  // )
+  composeEnhancers(applyMiddleware(...middleware))
 );
+
 export default store;
