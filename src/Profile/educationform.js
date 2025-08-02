0e5c4474d876e6cc25b07d5aab15a4113647f1e7
Dharminder Pal (CBT) <cbt.sd4@deepakfasteners.in>
2025-08-02 12:51:27 +0530

    onlyform

--- src/Profile/educationform.js
+++ src/Profile/educationform.js
@@ -157,7 +157,7 @@ const EducationForm = () => {
 
   return (
     <div className="education-form">
-      <h2>Add your Education Details</h2>
+          <h2>Add your Education Details</h2>
       
       <div className="current-education">
         <input
